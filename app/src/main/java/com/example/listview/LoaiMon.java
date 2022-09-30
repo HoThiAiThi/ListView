0ecbda6423d808aecec3e89709bf0e46be522782
HoThiAiThi <hothiaithi123@gmail.com>
2022-09-30 08:22:44 +0700

    listView

---
+++ app/src/main/java/com/example/listview/LoaiMon.java
@@ -0,0 +1,13 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoaiMon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loai_mon);
+    }
+}
